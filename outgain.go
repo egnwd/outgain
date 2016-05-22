9c8c985f26263ccdcc185bed4629166c902af232
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2016-05-22 17:24:41 +0100

    Move to gin router, Implement signin POST to log token

--- outgain.go
+++ outgain.go
@@ -1,53 +1,30 @@
 package main
 
 import (
-	"html/template"
-	"io/ioutil"
+	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// Entry point for the application, set up the database, server here
-	http.Handle("/", appHandler(hello))
-
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.ListenAndServe(":8080", nil)
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", hello)
+	router.POST("/signin", signIn)
+	router.StaticFS("/static", http.Dir("static"))
+	router.Run()
 }
 
-type httpError struct {
-	Error   error
-	Message string
-	Code    int
+func hello(c *gin.Context) {
+	c.HTML(http.StatusOK, "base.tmpl", gin.H{
+		"Title":   "Hello",
+		"Message": "Hello, World!",
+	})
 }
 
-type page struct {
-	Title,
-	Message string
-}
-
-type appHandler func(http.ResponseWriter, *http.Request) *httpError
-
-func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if e := fn(w, r); e != nil {
-		http.Error(w, e.Message, e.Code)
-	}
-}
-
-func hello(w http.ResponseWriter, r *http.Request) *httpError {
-	p := page{Title: "Hello", Message: "Hello, World!"}
-	html, err := ioutil.ReadFile("templates/base.tmpl")
-	if err != nil {
-		return &httpError{Error: err, Message: "Page Not Found", Code: 404}
-	}
-	tmpl, err := template.New("Hello").Parse(string(html))
-	if err != nil {
-		return &httpError{Error: err, Message: "Internal Server Error", Code: 500}
-	}
-	if err = tmpl.Execute(w, p); err != nil {
-		return &httpError{Error: err, Message: "Internal Server Error", Code: 500}
-	}
-
-	return nil
+func signIn(c *gin.Context) {
+	user := fmt.Sprintf("Current User: %s", c.PostForm("idtoken"))
+	log.Println(user)
 }
