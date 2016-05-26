058149cad7e339d083eedcf957823ee89f54814d
James  Lane <jrl14@imperial.ac.uk>
2016-05-26 18:33:49 +0100

    formatted sprites

--- server/controller/sprites.go
+++ server/controller/sprites.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-  "strings"
-  "strconv"
+	"strconv"
+	"strings"
 )
 
 func SpriteHandler(w http.ResponseWriter, r *http.Request, staticDir string) {
@@ -37,16 +37,16 @@ func SpriteHandler(w http.ResponseWriter, r *http.Request, staticDir string) {
 	stride := colouredSprite.Stride
 	pixels := colouredSprite.Pix
 
-  // Extract hex code of colour from the URL and convert to an integer
-  col := strings.TrimSuffix(strings.SplitAfter(r.URL.String(), "-")[1], ".png")
-  colVal, err := strconv.ParseUint(col, 16, 32)
+	// Extract hex code of colour from the URL and convert to an integer
+	col := strings.TrimSuffix(strings.SplitAfter(r.URL.String(), "-")[1], ".png")
+	colVal, err := strconv.ParseUint(col, 16, 32)
 	if err != nil {
 		log.Println(err)
 	}
-  // Get individual RGB values from hex colour code
-  r1 := uint8((colVal & 0xff0000) >> 16)
-  g1 := uint8((colVal & 0x00ff00) >> 8)
-  b1 := uint8(colVal & 0x0000ff)
+	// Get individual RGB values from hex colour code
+	r1 := uint8((colVal & 0xff0000) >> 16)
+	g1 := uint8((colVal & 0x00ff00) >> 8)
+	b1 := uint8(colVal & 0x0000ff)
 
 	// Iterate through pixels and colourise them
 	for i := minX; i < maxX; i++ {
@@ -59,30 +59,30 @@ func SpriteHandler(w http.ResponseWriter, r *http.Request, staticDir string) {
 			b0 := uint8(b32)
 			a0 := uint8(a32)
 			current := (j-minY)*stride + (i-minX)*4
-      // Set new pixel values, retaining white border
-      if (r0 == g0 && r0 == b0 && r0 == 0xff) {
-        pixels[current] = r0
-        pixels[current+1] = g0
-        pixels[current+2] = b0
-        pixels[current+3] = a0
-      } else {
-        // Uncomment to make colours more saturated
-        /*
-        if (r1 >= g1 && r1 >= b1) {
-          r1 = 0xff
-        }
-        if (g1 >= r1 && g1 >= b1) {
-          g1 = 0xff
-        }
-        if (b1 >= r1 && b1 >= g1) {
-          b1 = 0xff
-        }
-        */
-			  pixels[current] = r0 / 2 + r1 / 2
-			  pixels[current+1] = g0 / 2 + g1 / 2
-			  pixels[current+2] = b0 / 2 + b1 / 2
-			  pixels[current+3] = a0
-      }
+			// Set new pixel values, retaining white border
+			if r0 == g0 && r0 == b0 && r0 == 0xff {
+				pixels[current] = r0
+				pixels[current+1] = g0
+				pixels[current+2] = b0
+				pixels[current+3] = a0
+			} else {
+				// Uncomment to make colours more saturated
+				/*
+				   if (r1 >= g1 && r1 >= b1) {
+				     r1 = 0xff
+				   }
+				   if (g1 >= r1 && g1 >= b1) {
+				     g1 = 0xff
+				   }
+				   if (b1 >= r1 && b1 >= g1) {
+				     b1 = 0xff
+				   }
+				*/
+				pixels[current] = r0/2 + r1/2
+				pixels[current+1] = g0/2 + g1/2
+				pixels[current+2] = b0/2 + b1/2
+				pixels[current+3] = a0
+			}
 		}
 	}
 	// Write image to file
