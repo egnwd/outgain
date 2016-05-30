f7aae060b6eb3dca1e5d7148f081bd037862de9c
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2016-05-30 15:16:06 +0100

    Create (Dummy) Lobby Controller methods

---
+++ server/controller/lobbies.go
@@ -0,0 +1,55 @@
+package controller
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+func LobbiesView(w http.ResponseWriter, r *http.Request) {
+	if !IsUserAuthorised(r) {
+		u := fmt.Sprintf("http://%s/", r.Host)
+		http.Redirect(w, r, u, http.StatusFound)
+		return
+	}
+
+	id := strconv.FormatUint(uint64(rand.Uint32()), 10)
+	form := url.Values{}
+	form.Add("id", id)
+
+	u := fmt.Sprintf("http://%s/lobbies/join", r.Host)
+
+	http.Post(u, "application/x-www-form-urlencoded", strings.NewReader(form.Encode()))
+
+	u = fmt.Sprintf("http://%s/lobbies/%s", r.Host, id)
+	http.Redirect(w, r, u, http.StatusFound)
+}
+
+func LobbiesJoin(w http.ResponseWriter, r *http.Request) {
+	if !IsUserAuthorised(r) {
+		http.Error(w, "Not logged in.", http.StatusUnauthorized)
+	}
+
+	username, _ := GetUserName(r)
+	id := r.PostFormValue("id")
+	log.Printf("User: %s Joined Lobby: %s", username, id)
+}
+
+func LobbiesGame(w http.ResponseWriter, r *http.Request) {
+	if !IsUserAuthorised(r) {
+		u := fmt.Sprintf("http://%s/", r.Host)
+		http.Redirect(w, r, u, http.StatusFound)
+		return
+	}
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	fmt.Fprintln(w, id)
+}
