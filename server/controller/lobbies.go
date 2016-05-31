65cc563a02c5f626cde5effcf9d3a6600a6584d3
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2016-05-31 11:56:32 +0100

    Use Post request to join the lobby

--- server/controller/lobbies.go
+++ server/controller/lobbies.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 
 	"github.com/egnwd/outgain/server/lobby"
 	"github.com/egnwd/outgain/server/user"
@@ -24,17 +25,10 @@ func LobbiesView(w http.ResponseWriter, r *http.Request) {
 	form := url.Values{}
 	form.Add("id", fmt.Sprintf("%d", id))
 
-	username, _ := GetUserName(r)
-	user := user.NewUser(username)
-	err := lobby.AddUser(user)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	u := fmt.Sprintf("http://%s/lobbies/join", r.Host)
+	http.Post(u, "application/x-www-form-urlencoded", strings.NewReader(form.Encode()))
 
-	log.Printf("User: %s Joined Lobby: %d", username, id)
-
-	u := fmt.Sprintf("http://%s/lobbies/%d", r.Host, id)
+	u = fmt.Sprintf("http://%s/lobbies/%d", r.Host, id)
 	http.Redirect(w, r, u, http.StatusFound)
 }
 
@@ -51,7 +45,6 @@ func LobbiesJoin(w http.ResponseWriter, r *http.Request) {
 
 	l, ok := lobby.GetLobby(id)
 	if !ok {
-		log.Println("Not OK")
 		http.Error(w, "Lobby doesn't exist", http.StatusBadRequest)
 		return
 	}
