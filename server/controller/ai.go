223dcaea87fa4bdd0edc47b034358e96851e0ece
Paul Lietar <paul@lietar.net>
2016-06-03 15:26:19 +0100

    Add AI editor

---
+++ server/controller/ai.go
@@ -0,0 +1,39 @@
+package controller
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/egnwd/outgain/server/lobby"
+	"github.com/gorilla/mux"
+)
+
+func GetAISource() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		lobbyId, _ := strconv.ParseUint(vars["id"], 10, 64)
+
+		username, err := GetUserName(r)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "No username", http.StatusUnauthorized)
+			return
+		}
+
+		lobby, ok := lobby.GetLobby(uint64(lobbyId))
+		if !ok {
+			http.Error(w, "Lobby doesn't exist", http.StatusNotFound)
+			return
+		}
+
+		user := lobby.FindGuest(username)
+		if user == nil {
+			http.Error(w, "User not in lobby", http.StatusNotFound)
+			return
+		}
+
+		io.WriteString(w, user.Source)
+	})
+}
--- server/controller/lobbies.go
+++ server/controller/lobbies.go
@@ -8,7 +8,6 @@ import (
 	"strconv"
 
 	"github.com/egnwd/outgain/server/config"
-	"github.com/egnwd/outgain/server/guest"
 	"github.com/egnwd/outgain/server/lobby"
 	"github.com/gorilla/mux"
 )
@@ -61,10 +60,6 @@ func LobbiesGetUsers(w http.ResponseWriter, r *http.Request) {
 }
 
 func LobbiesJoin(w http.ResponseWriter, r *http.Request) {
-	if !IsUserAuthorised(r) {
-		http.Error(w, "Not logged in.", http.StatusUnauthorized)
-	}
-
 	// Get the id of the requested lobby
 	id, err := strconv.ParseUint(r.PostFormValue("id"), 10, 64)
 	if err != nil {
@@ -87,9 +82,8 @@ func LobbiesJoin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//Add the user to the lobby
-	user := guest.NewUser(username)
-	l.AddUser(user)
+	// Add the user to the lobby
+	l.AddUser(username)
 
 	l.Start()
 
@@ -128,10 +122,6 @@ func LobbiesGame(staticDir string) http.Handler {
 
 // LobbiesLeave temporarily logs the user out - this will change in the future
 func LobbiesLeave(w http.ResponseWriter, r *http.Request) {
-	if !IsUserAuthorised(r) {
-		http.Error(w, "Not logged in.", http.StatusUnauthorized)
-	}
-
 	// Get the id of the requested lobby
 	id, err := strconv.ParseUint(r.PostFormValue("id"), 10, 64)
 	if err != nil {
@@ -155,8 +145,7 @@ func LobbiesLeave(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Remove the user to the lobby
-	user := guest.NewUser(username)
-	l.RemoveUser(user)
+	l.RemoveUser(username)
 
 	// Redirect user to the lobby
 	log.Printf("User: %s Left Lobby: %d", username, id)
