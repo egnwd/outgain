524bd8611c6233c2d4b38aa2a058390f5b9b31d4
Michael Radigan <michael.radigan14@imperial.ac.uk>
2016-06-08 04:30:01 +0100

    Extended entities interface to include IsUser (shoud I change some values to private or just leave as they are?)

--- server/engine/entities.go
+++ server/engine/entities.go
@@ -29,6 +29,7 @@ type Entity interface {
 	GetName() string
 	GetGains() int
 	Close()
+	IsUser() bool
 }
 
 type EntityBase struct {
@@ -211,6 +212,13 @@ func (resource *Resource) BonusFactor() float64 {
 	return resourceBonusFactor
 }
 
+func (resource *Resource) IsUser() bool {
+	return false
+}
+
+func (resource *Resource) Close() {
+}
+
 type Spike struct {
 	EntityBase
 }
@@ -267,5 +275,6 @@ func (spike *Spike) GetName() string {
 	return "spike"
 }
 
-func (resource *Resource) Close() {
+func (spike *Spike) IsUser() bool {
+	return false
 }
