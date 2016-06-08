71c67164f601cc7b70a7310412beb21894704334
Michael Radigan <michael.radigan14@imperial.ac.uk>
2016-06-08 05:26:10 +0100

    Pretty colours, should probably extract into CSS

--- client/src/main.ts
+++ client/src/main.ts
@@ -59,25 +59,34 @@ $(function() {
         let logEvent = <ILogEvent>data
 
         let scrollUpdate = gameLog.scrollHeight - gameLog.clientHeight <= gameLog.scrollTop + 1
-
+	// The colours should probbaly be done with CSS,
+	// leaving it here so that someone who cares more
+	// than me can play with them easier
       	switch (logEvent.logType) {
       	    case 0:
-      		gameLog.innerHTML = "A new game has started, good luck!\n"
+      		gameLog.innerHTML = "A new game has started, good luck!\n" 
       	        break
       	    case 1:
-      		gameLog.innerHTML = gameLog.innerHTML + "Yum, "
-      		    + logEvent.protagName + " ate a resource\n"
+      	        gameLog.innerHTML = gameLog.innerHTML
+		+ "<span style='color:#85b735'>" // Matches --success:hover
+		+ "Yum, "+ logEvent.protagName
+		+ " ate a resource\n" + "</span>"
       		break
       	    case 2:
       		gameLog.innerHTML = gameLog.innerHTML
-      		    + logEvent.protagName + " ate " + logEvent.antagName + "\n"
+		+ "<span style='color:#4022FF'>" 
+		+ logEvent.protagName + " ate " + logEvent.antagName
+		+ "\n" + "</span>"
       		break
             case 3:
-	        gameLog.innerHTML = gameLog.innerHTML + "Oh no, creature " 
-		    + logEvent.protagName + " hit a spike!\n"
+	        gameLog.innerHTML = gameLog.innerHTML
+	        + "<span style='color:#d0131C'>" // Matches --fail:hover
+		+ "Oh no, creature " 
+		+ logEvent.protagName + " hit a spike!\n"
+		+  "</span>"
       	}
 
-        if (userPanel.getUserID() == logEvent.protagName) {
+        if (userPanel.username == logEvent.protagName) {
           let user_gains = document.getElementById("user-gains")
           user_gains.innerHTML = logEvent.gains.toString()
         }
