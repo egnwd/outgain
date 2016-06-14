0d8f8a20374a7646db84c3241ea2141d1eaf0145
Paul Lietar <plietar@users.noreply.github.com>
2016-06-14 11:13:33 +0100

    Change default_ai to be the circle one

--- default_ai.rb
+++ default_ai.rb
@@ -1,32 +1,5 @@
-angle = rand * 2 * Math::PI
-@dx = Math::cos(angle)
-@dy = Math::sin(angle)
-
 def run(player, world)
-    angle = rand * Math::PI / 2 - Math::PI / 4
-
-    c = Math::cos(angle)
-    s = Math::sin(angle)
-
-    dx = c * @dx - s * @dy
-    dy = s * @dx + c * @dy
-
-    @dx = dx
-    @dy = dy
-
-    prey = world.entities
-        .select {|e| e.resource? or (e.creature? and e.id != player.id and e < player) }
-        .min_by {|e| player.distance e}
-
-    predator = world.entities
-        .select {|e| e.spike? or (e.creature? and e.id != player.id and e > player) }
-        .min_by {|e| player.distance e}
-
-    if prey != nil then
-        return [prey.x - player.x, prey.y - player.y]
-    elsif predator != nil then
-        return [player.x - predator.x, player.y - predator.y]
-    else
-        return [@dx, @dy]
-    end
+  cx = 10
+  cy = 10
+  [-(player.y - cy), (player.x - cx)]
 end
